592e04b82fc8b0a86e90e6f45c04c0833fa3145b
Johnny Wan <johnnywon@g.ucla.edu>
2020-01-26 17:31:16 -0800

    complete super powerful command

--- very_useful_git_command.sh
+++ very_useful_git_command.sh
@@ -1,5 +1,41 @@
 #! /bin/bash
 
 git filter-branch --force --index-filter \
-  'git rm --cached --ignore-unmatch Lab_3/README' \
+  'git rm --cached --ignore-unmatch Lab_0/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_1a/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_1b/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_2a/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_2b/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_3a/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_3b/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_4a/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_4b/README' \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch Lab_4c/README' \
   --prune-empty --tag-name-filter cat -- --all

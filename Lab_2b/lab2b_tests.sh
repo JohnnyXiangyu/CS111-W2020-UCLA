f10a86171aa1190887180d279d496cd977dbd833
Johnny Wan <johnnywon@g.ucla.edu>
2020-02-16 16:35:19 -0800

    combine 2.3.1 and 2.3.2 test cases together (since they are the same set of tests)

--- Lab_2b/lab2b_tests.sh
+++ Lab_2b/lab2b_tests.sh
@@ -5,13 +5,7 @@ echo "" > lab2b_list.csv
 
 for i in 1 2 4 8 12 16 24
 do
-    ./lab2_list --threads=$i --iterations=1000 --sync=m >> lab2b_list.csv
+    ./lab2_list --threads=$i --iterations=1000 --sync=m >> lab2b_list.csv # same line used for 2.3.3
     ./lab2_list --threads=$i --iterations=1000 --sync=s >> lab2b_list.csv
 done
 
-
-# Q2.3.3
-for i in 1 2 4 8 16 24
-do
-    ./lab2_list --threads=$i --iterations=1000 --sync=m >> lab2b_list.csv
-done

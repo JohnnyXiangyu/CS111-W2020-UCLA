f03856bb1555e4b107f16cad07b08e95a688eae3
Johnny Wan <johnnywon@g.ucla.edu>
2020-02-16 16:07:51 -0800

    generate data for lab2b_2.png

--- Lab_2b/lab2b_tests.sh
+++ Lab_2b/lab2b_tests.sh
@@ -8,3 +8,10 @@ do
     ./lab2_list --threads=$i --iterations=1000 --sync=m >> lab2b_list.csv
     ./lab2_list --threads=$i --iterations=1000 --sync=s >> lab2b_list.csv
 done
+
+
+# Q2.3.3
+for i in 1 2 4 8 16 24
+do
+    ./lab2_list --threads=$i --iterations=1000 --sync=m >> lab2b_list.csv
+done

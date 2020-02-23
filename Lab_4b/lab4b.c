4f02a0db9c4cf5b8d5d9dbdd6cf7baf5c8bab7b9
Johnny Wan <wonjohnny@outlook.com>
2020-02-22 16:47:21 -0800

    button example added

--- Lab_4b/lab4b.c
+++ Lab_4b/lab4b.c
@@ -5,38 +5,37 @@
 sig_atomic_t volatile run_flag = 1;
 int debug_flag = 0; /* flag debug option */
 
-void intHandler(int sig) {
-    if (sig == SIGINT) {
-        run_flag = 0;
-    }
+void intHandler() {
+    run_flag = 0;
 }
 
 int main() {
     printf("this is the test version\n");
 
-    /* register a gpio context, for port 62, name buzzer */
-    mraa_gpio_context buzzer;
+    /* register a gpio context, for port 62, name buzzer 
+       and */
+    mraa_gpio_context buzzer, button;
     buzzer = mraa_gpio_init(62);
+    button = mraa_gpio_init(60);
     
     /* configure buzzer to output pin */
     mraa_gpio_dir(buzzer, MRAA_GPIO_OUT);
+    mraa_gpio_dir(button, MRAA_GPIO_IN);
 
     /* register a SIGINT handler */
     signal(SIGINT, intHandler);
+    mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, &intHandler, NULL);
 
     while (run_flag) {
         /* buzzer on */
-        mraa_gpio_write(buzzer, 1);
-        sleep(1);
-
-        /* buzzer off */
-        mraa_gpio_write(buzzer, 0);
+        printf("buzzzz\n");
         sleep(1);
     }
 
     /* turn off buzzer and close context */
     mraa_gpio_write(buzzer, 0);
     mraa_gpio_close(buzzer);
+    mraa_gpio_close(button);
 
     return 0;
 }

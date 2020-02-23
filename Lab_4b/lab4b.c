0e62c764fde79b43fa2073ccb3fffc617c4c9c33
Johnny Wan <wonjohnny@outlook.com>
2020-02-22 17:09:17 -0800

    add code to read analog input

--- Lab_4b/lab4b.c
+++ Lab_4b/lab4b.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <mraa/gpio.h>
+#include <mraa/aio.h>
 
 sig_atomic_t volatile run_flag = 1;
 int debug_flag = 0; /* flag debug option */
@@ -12,22 +13,27 @@ void intHandler() {
 int main() {
     printf("this is the test version\n");
 
-    /* register a gpio context, for port 62, name buzzer 
-       and */
+    uint16_t analog_in;
+
+    /* register a gpio context, for pin 60, name button */
     mraa_gpio_context button;
     button = mraa_gpio_init(60);
+
+    /* register a AIO context, for pin 1, name t_sensor */
+    mraa_aio_context t_sensor;
+    t_sensor = mraa_aio_init(1);
     
     /* configure buzzer to output pin */
     mraa_gpio_dir(button, MRAA_GPIO_IN);
 
-    /* register a SIGINT handler */
+    /* register a button handler */
     signal(SIGINT, intHandler);
     mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, &intHandler, NULL);
 
     while (run_flag) {
         /* buzzer on */
-        printf("buzzzz\n");
-        sleep(1);
+        analog_in = mraa_aio_read(t_sensor);
+        printf("%d\n", analog_in);
     }
 
     /* close context */

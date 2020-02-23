651273d5ca53b9c3f87c4d90029040b43177f4be
Johnny Wan <wonjohnny@outlook.com>
2020-02-23 00:16:16 -0800

    for now parsing function not working

--- Lab_4b/lab4b.c
+++ Lab_4b/lab4b.c
@@ -7,7 +7,9 @@
 #include <mraa/gpio.h>
 #include <mraa/aio.h>
 #include <stdlib.h>
+#include <sys/poll.h>
 
+sig_atomic_t volatile on_flag = 1;
 sig_atomic_t volatile run_flag = 1;
 
 static char* error_message =
@@ -35,6 +37,10 @@ int log_flag = 0; /* whether to write into a file, default 0 */
 static char* log_filename = ""; /* file name to write into */
 FILE* log_file = NULL; /* log target of fprintf */
 
+/* buffering stdin reading */
+char read_buf[1024]; /* buffer for reading stdin */
+char swap_buf[1024]; /* buffer for temporarily store contents of read_buf */
+int processed_byte = 0; /* indicating where did last search end, should always point to a \n */
 
 
 /* separate function that prints formatted time */
@@ -71,12 +77,12 @@ void finalize() {
 
 /* handler for interruption (button) */
 void intHandler() {
-    if (run_flag) {
-        run_flag = 0;
+    if (on_flag) {
+        on_flag = 0;
         printTime();
-        printf(" OFF\n");
+        printf(" SHUTDOWN\n");
         if (log_flag) {
-            fprintf(log_file, " OFF\n");
+            fprintf(log_file, " SHUTDOWN\n");
         }
 
         /* wrap and exit */
@@ -100,7 +106,95 @@ float convertTemp(int a) {
 }
 
 
+/* parse read_buf and execute the commands */
+int parseReadBuf() {
+    if (debug_flag) { fprintf(stderr, "parsing\n"); }
+
+    /* loop read_buffer and replace all newlines with zero-byte */
+    int i = 0;
+    int max = strlen(read_buf);
+    processed_byte = 0;
+    for (i = 0; i < max; i++) {
+        if (read_buf[i] == '\n') {
+            read_buf[i] = '0';
+            processed_byte = i;
+        }
+    }
+
+    /* loop each segment of read_buf */
+    i = 0;
+    while (i < processed_byte) {
+        int cur_len = strlen(&read_buf[i]);
+
+        if (strcmp("scale=F", &read_buf[i]) == 0) {
+            scale = 'F';
+            if (debug_flag) { fprintf(stderr, "F\n"); }
+        }
+        else if (strcmp("scale=C", &read_buf[i]) == 0) {
+            scale = 'C';
+            if (debug_flag) { fprintf(stderr, "C\n"); }
+        }
+        else if (strcmp("STOP", &read_buf[i]) == 0) {
+            run_flag = 0;
+            if (debug_flag) { fprintf(stderr, "S\n"); }
+        }
+        else if (strcmp("START", &read_buf[i]) == 0) {
+            run_flag = 1;
+            if (debug_flag) { fprintf(stderr, "S\n"); }
+        }
+        else if (strcmp("OFF", &read_buf[i]) == 0) {
+            on_flag = 0;
+            if (debug_flag) { fprintf(stderr, "O\n"); }
+        }
+        else if (cur_len >=7 && strncmp("PERIOD=", &read_buf[i], 7) == 0) {
+            int new_period = atoi(&read_buf[i] + 7);
+            period = new_period;
+            if (debug_flag) { fprintf(stderr, "P\n"); }
+        }
+
+        if (log_flag && log_file != NULL) {
+            fprintf(log_file, "%s", &read_buf[i]);
+        }
+
+        i += cur_len;
+    }
+
+    return i;
+}
+
+
+/* read input from stdin and append to read_buf */
+int m_read() {
+    if (debug_flag) { fprintf(stderr, "reading\n"); }
+
+    int cur_bytes = strlen(read_buf);
+    int avail_bytes = 1023 - cur_bytes;
+    int new_bytes = read(STDIN_FILENO, &read_buf[cur_bytes], avail_bytes);
+    if (new_bytes == -1) {
+        fprintf(stderr, "ERROR: read() return -1, exiting...\n");
+        finalize();
+        exit(2);
+    }
+    else if (new_bytes > 0) {
+        read_buf[cur_bytes + new_bytes] = '\0';
+    }
+
+    parseReadBuf();
+
+    cur_bytes = 1023 - processed_byte;
+    strcpy(swap_buf, &read_buf[processed_byte + 1]);
+    swap_buf[cur_bytes] = '\0';
+    strcpy(read_buf, swap_buf);
+    read_buf[cur_bytes] = '\0';
+
+    return cur_bytes;
+}
+
+
 int main(int argc, char **argv) {
+    /* initialize read_buf to empty string */
+    read_buf[0] = '\0';
+
     /* precess args */
     struct option options[7] = {
         {"period", required_argument, 0, 'p'},
@@ -144,36 +238,55 @@ int main(int argc, char **argv) {
             break;
         }
     }
-
     if (log_flag) {
         log_file = fopen(log_filename, "w+");
+        if (log_file == NULL) {
+            fprintf(stderr, "ERROR: opening file %s failed, exiting...\n", log_filename);
+            exit(1);
+        }
     }
 
-    /* register a gpio context, for pin 60, name button */
-    button = mraa_gpio_init(butPin);
+    // prepare to poll()
+    struct pollfd fds;
+    fds.fd = STDIN_FILENO;
+    fds.events = POLLIN;
 
-    /* register a AIO context, for pin 1, name t_sensor */
-    t_sensor = mraa_aio_init(senPin);
+    button = mraa_gpio_init(butPin); /* register a gpio context, for pin 60, name button */
+    t_sensor = mraa_aio_init(senPin); /* register a AIO context, for pin 1, name t_sensor */
     
-    /* configure buzzer to output pin */
-    mraa_gpio_dir(button, MRAA_GPIO_IN);
+    mraa_gpio_dir(button, MRAA_GPIO_IN); /* configure buzzer to output pin */
 
     /* register a button handler */
-    signal(SIGINT, intHandler);
+    signal(SIGINT, intHandler); // should sigint be registered?
     mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, &intHandler, NULL);
 
-    while (run_flag) {
-        /* readin analog input */
-        int analog_in = mraa_aio_read(t_sensor);
-        /* convert raw input to temperature C */
-        float cur_temp = convertTemp(analog_in);
+    while (on_flag) {
+        if (run_flag) {
+            /* read in analog input */
+            int analog_in = mraa_aio_read(t_sensor);
+            /* convert raw input to temperature C */
+            float cur_temp = convertTemp(analog_in);
 
-        printTime();
-        printf(" ");
-        if (log_flag) { fprintf(log_file, " "); }
-        printTemp(cur_temp);
-        printf("\n");
-        if (log_flag) { fprintf(log_file, "\n"); }
+            printTime();
+            printf(" ");
+            if (log_flag) { fprintf(log_file, " "); }
+            printTemp(cur_temp);
+            printf("\n");
+            if (log_flag) { fprintf(log_file, "\n"); }
+        }
+
+        /* read and execute stdin */
+        int rc = poll(&fds, 1, 0);
+        if (rc == -1) {
+            fprintf(stderr, "ERROR: poll() failed, exiting...\n");
+            finalize();
+            exit(1);
+        }
+        else if (rc > 0) {
+            if (fds.revents & POLL_IN) { /* if there's input from stdin */
+                m_read();
+            }
+        }
 
         /* wait for some time */
         sleep(period);
